d437d5f454d8ae2e173ce4b844ce9bda8ad8ae3e
Isa del Aguila <imaguila@ual.es>
2026-06-03 19:30:39 +0200

    ff

---
+++ imgs/grafo.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+
+# Ejemplo (rellena con tus datos reales)
+requirements = [
+    'Save', 'New', 'Open', 'Close', 'Print',
+    'Copy', 'Paste', 'Undo', 'Redo', 'Cut'
+]
+
+moscow_M = [12, 11, 11, 10, 8, 8, 8, 6, 6, 6]
+
+# EJEMPLO: sustituir por frecuencia real del heatmap
+css_freq = [10, 9, 9, 8, 7, 8, 8, 9, 9, 7]
+
+fig, ax1 = plt.subplots()
