b06ad0c715fb1ce6da536c1b21955295647668b2
Isa del Aguila <imaguila@ual.es>
2026-06-03 09:23:08 +0200

    asdf

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 PROBLEMAS = {
 
-    "BAGNALL": {
+    "CLASSIC Dataset": {
         "path_sol": "data/BAGNALL/bagnallsoluciones.txt",
         "path_prob": "data/BAGNALL/bagnalldataset.txt",
         "metricas": ["satisfaction", "effort"],
@@ -11,10 +11,10 @@ PROBLEMAS = {
             "productivity",
             "squandering",
         ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Software release planning: an evolutionary and iterative approach",
     },
 
-    "MSLITE": {
+    "MSLite System": {
         "path_sol": "data/MSLITE/mslitesoluciones.txt",
         "path_prob": None,
         "metricas": ["satisfaction", "effort", "dissatisfaction"],
@@ -27,10 +27,10 @@ PROBLEMAS = {
             "annoyance",
             "dirtiness",
           ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Sangwan, R. S., Negahban, A., Nord, R. L., & Ozkaya, I. (2020). Optimization of software release planning considering architectural dependencies, cost, and value. IEEE Transactions on Software Engineering, 48(4), 1369-1384.",
     },
 
-    "RALICSREQ": {
+    "Replacement Access, Library and ID Card (RALIC)": {
         "path_sol": "data/RALICSreq/ralicsreqsoluciones.txt",
         "path_prob": "data/RALICSreq/ralicsreqdataset.txt",
         "metricas": ["satisfaction", "effort"],
@@ -41,10 +41,10 @@ PROBLEMAS = {
             "productivity",
             "squandering",
         ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Lim, S. L., & Finkelstein, A. (2011). StakeRare: using social networks and collaborative filtering for large-scale requirements elicitation. IEEE transactions on software engineering, 38(3), 707-735.",
     },
 
-    "WORDPROC": {
+    "Word processing software project": {
         "path_sol": "data/WORDPROC/wordprocsoluciones.txt",
         "path_prob": "data/WORDPROC/wordprocdataset.txt",
         "metricas": ["satisfaction", "effort", "time"],
@@ -57,9 +57,9 @@ PROBLEMAS = {
             "response",
             "opportunity",
         ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Agarwal, N., Karimpour, R., & Ruhe, G. (2014, January). Theme-based product release planning: An analytical approach. In 2014 47th Hawaii International Conference on System Sciences (pp. 4739-4748). IEEE.",
     },
-    "REQ100": {
+    "Large dataset": {
         "path_sol": "data/REQ100/req100frente.txt",
         "path_prob": "data/REQ100/req100dataset.txt",
         "metricas": ["satisfaction", "effort"],
@@ -70,9 +70,9 @@ PROBLEMAS = {
             "productivity",
             "squandering",
          ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Del Sagrado, J., Del Águila, I. M., & Orellana, F. J. (2015). Multi-objective ant colony optimization for requirements selection. Empirical Software Engineering, 20(3), 577-610.",
     },
-    "THEME": {
+    "ReleasePlanner™ dataset": {
         "path_sol": "data/THEME/themesoluciones.txt",
         "path_prob": "data/THEME/themedataset.txt",
         "metricas": [
@@ -93,9 +93,9 @@ PROBLEMAS = {
             "robustness",
             "usage_efficiency",
         ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Karim, M. R., & Ruhe, G. (2014, August). Bi-objective genetic search for release planning in support of themes. In International Symposium on Search Based Software Engineering (pp. 123-137). Cham: Springer International Publishing.",
     },
-    "MOTOROLA": {
+    "Motorola Dataset": {
         "path_sol": "data/MOTOROLA/motorolasoluciones.txt",
         "path_prob": "data/MOTOROLA/motoroladataset.txt",
         "metricas": ["satisfaction", "effort"],
@@ -106,6 +106,6 @@ PROBLEMAS = {
             "productivity",
             "squandering",
         ],
-        "paper": "Bagnall, A. J., Rayward-Smith, V. J., & Whittley, I. M. (2001). The next release problem. Information and software technology, 43(14), 883-890.",
+        "paper": "Baker, P., Harman, M., Steinhofel, K., & Skaliotis, A. (2006, September). Search based approaches to component selection and prioritization for the next release problem. In 2006 22nd IEEE International Conference on Software Maintenance (pp. 176-185). IEEE.",
     }
 }
